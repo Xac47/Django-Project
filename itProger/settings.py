e748d63ec00da6d664aeb7d295fb420ef646c75f
Xac47 <hasbulat69@gmail.com>
2022-12-05 00:20:46 +0300

    Изменение в блоке "Мои статьи" а именно отображение количество статьи

--- itProger/settings.py
+++ itProger/settings.py
@@ -122,10 +122,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'hasbulat69@gmail.com'
+EMAIL_HOST_PASSWORD = 'wgelfydmfetubzop'
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    (BASE_DIR / 'static')
+]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = (BASE_DIR / 'media')
