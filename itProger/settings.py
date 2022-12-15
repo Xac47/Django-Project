59eaf14b1b2e6c9bab61727d31058ee7acf14d9a
Xac47 <hasbulat69@gmail.com>
2022-12-15 23:13:55 +0300

    Изменение профиля

--- itProger/settings.py
+++ itProger/settings.py
@@ -124,8 +124,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'hasbulat69@gmail.com'
-EMAIL_HOST_PASSWORD = 'wgelfydmfetubzop'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 STATIC_URL = 'static/'
 
